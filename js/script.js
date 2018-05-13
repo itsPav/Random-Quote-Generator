7ee6704145e09e89aac3aa4abe650524ae07dd2c
Pav <pavel-m@live.com>
2018-05-13 19:36:52 -0400

    Completed Version
    Added more quotes
    Fixed tags

--- js/script.js
+++ js/script.js
@@ -7,32 +7,32 @@ var intervalID = window.setInterval(printQuote, 10000);
 
 // array object of quotes
 var quotes = [
-{
-    'quote': 'But man is not made for defeat. A man can be destroyed but not defeated.',
-    'source': 'Ernest Hemingway'
-},
-{
-    'quote': 'quote2',
-    'source': 'source2',
-    'citation': 'citation',
-    'year': 1994,
-    'tag': ''
-},
-{
-    'quote': 'quote3',
-    'source': 'source3',
-    'citation': 'citation'
-},
-{
-    'quote': 'quote4',
-    'source': 'source3',
-    'citation': 'citation'
-},
-{
-    'quote': 'Hello World',
-    'source': 'Any Programming Book',
-    'tag': 'Humour'
-}
+    {
+        'quote': 'But man is not made for defeat. A man can be destroyed but not defeated.',
+        'source': 'Ernest Hemingway'
+    },
+    {
+        'quote': 'The only thing we have to fear is fear itself.',
+        'source': 'Franklin Delano Roosevelt',
+        'citation': 'FDR’s First Inaugural Address',
+        'year': 1933,
+        'tag': ['politics','president']
+    },
+    {
+        'quote': 'We simply attempt to be fearful when others are greedy and to be greedy only when others are fearful.',
+        'source': 'Warren Buffett',
+        'tag': ['finance','investing']
+    },
+    {
+        'quote': 'You can do anything but not everything',
+        'source': 'David Allen',
+        'citation': 'Making It All Work'
+    },
+    {
+        'quote': 'Hello World',
+        'source': 'Any Programming Book',
+        'tag': ['humour']
+    }
 ]
 
 // random quote variable
@@ -56,7 +56,8 @@ function changeBackgroundColor() {
 }
 
 // function to call getRandomQuote and to use the number generated to get the quote object
-function printQuote(){
+function printQuote() 
+{
     getRandomQuote();
     changeBackgroundColor();
 
@@ -76,14 +77,20 @@ function printQuote(){
         newQuote += '<span class="year">' + randomQuote.year + '</span>';
     }
 
-    // checking if object has year property
+
+
+    // checking if object has tag property
     if(randomQuote.hasOwnProperty("tag")){
-        // if it has a year, concatenate year to string
-        newQuote += '<span class="tag">' + randomQuote.tag + '</span>';
+        // check if quote has a year
+        if(randomQuote.hasOwnProperty("year")){
+            // if it has a year, add a comma to separate the tags
+            newQuote += ',';
+        }
+    
+        // if it has a tag, concatenate tag to string
+        newQuote += '<span class="tag">' + ' ' + randomQuote.tag.join(', ') + '</span>';
     }
 
     newQuote += '</p>';
-
     document.getElementById('quote-box').innerHTML = newQuote;
 }
-
